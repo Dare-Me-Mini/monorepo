19ba98ec86873b585a2985bf823f17353b3ddccb
Yash <contact@yashgoyal.dev>
2025-08-14 23:14:10 +0530

    added env template and updated some env variables in the indexer

--- packages/indexer/ponder.config.ts
+++ packages/indexer/ponder.config.ts
@@ -2,19 +2,23 @@ import { createConfig } from "ponder";
 
 import { BettingHouseAbi } from "./abis/BettingHouseAbi";
 
+if (!process.env.BASE_RPC_URL || !process.env.BETTING_HOUSE_ADDRESS) {
+  throw new Error("Missing environment variables");
+}
+
 export default createConfig({
   chains: {
-    mainnet: {
-      id: 1,
-      rpc: process.env.PONDER_RPC_URL_1!,
+    base: {
+      id: 8453,
+      rpc: process.env.BASE_RPC_URL,
     },
   },
   contracts: {
     BettingHouse: {
-      chain: "mainnet",
+      chain: "base",
       abi: BettingHouseAbi,
       address: process.env.BETTING_HOUSE_ADDRESS as `0x${string}`,
-      startBlock: Number(process.env.PONDER_START_BLOCK || 0),
+      startBlock: Number(process.env.PONDER_START_BLOCK || "0"),
     },
   },
 });
