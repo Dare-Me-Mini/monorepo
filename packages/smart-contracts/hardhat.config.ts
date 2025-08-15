b33387f8406c2681d45284558c58d3758fbc4a51
Yash <contact@yashgoyal.dev>
2025-08-15 14:28:44 +0530

    added base network support in the hardhat config

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -8,6 +9,12 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: { enabled: true, runs: 200 },
       viaIR: true,
+    },
+  },
+  networks: {
+    base: {
+      url: process.env.BASE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY || ""],
     },
   },
 };
